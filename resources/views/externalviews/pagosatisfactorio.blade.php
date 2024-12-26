aed99ee4345f57cc07262eff856e7314d2aa045b
typej2003 <typej2003@gmail.com>
2024-12-26 19:23:19 -0400

    optimo

--- resources/views/externalviews/pagosatisfactorio.blade.php
+++ resources/views/externalviews/pagosatisfactorio.blade.php
@@ -1,26 +1,9 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/app.css">
 
-@livewire('layouts.navbar-checkout')
 
 <div class="container-fluid">
     <div>
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0 text-dark"></h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="/admin/dashboard">Escritorio</a></li>
-                            <li class="breadcrumb-item active"><a href="/listPedidosCliente">Mis Pedidos</a></li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
@@ -29,21 +12,13 @@
                     <div class="col-lg-12">
                         <div class="card" style="width: 100% !important;">
                             <div class="card-body text-center">
-                                <h1>Operación procesada con éxito, qué desea hacer ahora?</h1>
+                                <h1>Operación procesada con éxito</h1>
+                                <h3>¿Qué desea hacer ahora?</h3>
                                 <p>
                                     <a target="_parent" class="h5 text-titulo" href="/">Seguir comprando</a>
                                 </p>
                                 <p >
-                                    <a target="_parent" class="h5 text-titulo" href="/listPedidosCliente">Mis Pedidos</a>
-                                </p>
-                                <p>
-                                    <button class="btn btn-success" id="salir">Salir</button>
-                                </p>
-                                <p>
-                                    <a target="_parent" href="/">Hacia el /</a>
-                                </p>
-                                <p>
-                                    <a target="_parent" href="https://www.panexpres.com">www.panexpres.com</a>
+                                    <a target="_parent" class="h5 text-titulo" href="/listPedidosCliente">Ir a Mis Pedidos</a>
                                 </p>
                             </div>
                             <div class="card-footer d-flex justify-content-end">
