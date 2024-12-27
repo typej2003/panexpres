87ef00e626c6781fc428b22b6f364ec4de5a67ad
typej2003 <typej2003@gmail.com>
2024-12-27 09:37:32 -0400

    promocion

--- storage/framework/views/73257d505a27f1fc35b9fd589f547fc7c5f6a34c.php
+++ storage/framework/views/73257d505a27f1fc35b9fd589f547fc7c5f6a34c.php
@@ -1,29 +1,29 @@
 <div>
     <style>
-        .img1 {
+        .img1Promocion {
             width: 100%;
             height: 100%;
         }
-        .img2 {
+        .img2Promocion {
             width: 100%;
             height: 100%;
         }
-        .cuadro1{
+        .cuadro1Promocion{
             width: 100%;
-            height: 300px;
+            height: 400px;
         }
-        .cuadro2{
+        .cuadro2Promocion{
             width: 100%;
-            height: 150px;
+            height: 200px;
         }
     </style>
     <div class="row">
         <div class="col-md-7">
-            <div class="bg-secundary cuadro1 p-2">
+            <div class="bg-secundary cuadro1Promocion p-2">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <a href="/viewdetails/2/2"><img class="img1" src="/img/promociones/panaderia_expreso_compressed.jpg" alt="First slide"></a>
+                            <a href="/viewdetails/2/2"><img class="img1Promocion" src="/img/promociones/panaderia_expreso_compressed.jpg" alt="First slide"></a>
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="..." alt="Second slide">
@@ -47,15 +47,15 @@
         <div class="col-md-5">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="bg-secundary cuadro2 p-2">
-                        <a href="/viewdetails/2/1"><img class="img2" src="/img/promociones/panaderia_expreso_derecha1_compressed.jpg" alt=""></a>
+                    <div class="bg-secundary cuadro2Promocion p-2">
+                        <a href="/viewdetails/2/1"><img class="img2Promocion" src="/img/promociones/panaderia_expreso_derecha1_compressed.jpg" alt=""></a>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <div class="bg-secundary cuadro2 p-2">
-                        <a href="viewdetails/2/2"><img class="img2" src="/img/promociones/panaderia_expreso_derecha2_compressed.jpg" alt=""></a>
+                    <div class="bg-secundary cuadro2Promocion p-2">
+                        <a href="viewdetails/2/2"><img class="img2Promocion" src="/img/promociones/panaderia_expreso_derecha2_compressed.jpg" alt=""></a>
                     </div>
                 </div>
             </div>
