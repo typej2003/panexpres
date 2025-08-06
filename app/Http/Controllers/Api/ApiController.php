f680dae6fc53a44491d56fbb6e18c38cc82c8ad0
typej2003 <typej2003@gmail.com>
2025-08-06 12:08:36 -0400

    corregir

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,8 +1,9 @@
 <?php
 
-    namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api;
 
-    use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
     class ApiController extends Controller
     {
