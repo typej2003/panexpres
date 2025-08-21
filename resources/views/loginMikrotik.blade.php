fb7bced91a49a29cc2ebc95c75c6259585a18423
typej2003 <typej2003@gmail.com>
2025-08-21 01:24:36 -0400

    css

--- resources/views/loginMikrotik.blade.php
+++ resources/views/loginMikrotik.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
 <body>
 <div>
