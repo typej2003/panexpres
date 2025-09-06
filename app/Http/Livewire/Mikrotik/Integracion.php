625ae4eada64444d054d4cdbcaf798661f05317b
Typej <typej2003@gmail.com>
2025-09-06 11:52:39 -0400

    actualizar

--- app/Http/Livewire/Mikrotik/Integracion.php
+++ app/Http/Livewire/Mikrotik/Integracion.php
@@ -15,63 +15,58 @@ class Integracion extends Component
     public $response = null;
     public $identity = null;
 
-    public function integracion($host, $user, $pass)
+    public function integracion($host='typej.ddns.net', $user = 'admin', $pass= 'admin123')
     {
-        // Iniciar la conexión
-        // $client = new Client([
-        // 'host' => '192.168.88.1',
-        // 'user' => 'tu_usuario_api',
-        // 'pass' => 'tu_contraseña_api'
-        // ]);
-        return new Client([
-            'host' => $host,
-            'user' => $user,
-            'pass' => $pass,
-            'port' => '8728',
-        ]);
+        try {
+            //prueba 1
+            $dominio = 'typej.ddns.net';
+            $host = gethostbyname($dominio);
+
+            //$host = '192.168.2.1';
+            $host = 'typej.ddns.net';
+            $user = 'admin';
+            $pass = 'admin123';
+
+            // Iniciar la conexión
+            $client = new Client([
+                'host' => $host,
+                'user' => $user,
+                'pass' => $pass,
+                // 'port' => 8729,
+            ]);
+            
+            $query = new Query('/system/identity/getall');
+
+            $identity = $client->query($query)->read();
+
+            $this->identity = $identity[0]['name'];
+
+            $query = new Query('/system/resource/print');
+
+            $response = $client->query($query)->read();
+            // La variable $response contendrá un array con la información de los usuarios
+
+            $claves = array_keys($response[0]);
+
+            //dd($response[0]);
+            $texto = '';
+
+            foreach ($response[0] as $key => $elemento) {
+                $texto .= $key .': '. $elemento . '<br>'; //PHP_EOL;
+            }
+
+            $this->response = $texto;
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            dd('error: ' . $th);
+        }        
 
     }
 
     public function mount()
     {
-        //prueba 1
-        $dominio = 'typej.ddns.net';
-        $host = gethostbyname($dominio);
-
-        //$host = '201.209.23.151';
-        $host = 'typej.ddns.net';
-        $user = 'admin';
-        $pass = 'admin123';
-
-        // Iniciar la conexión
-        $client = new Client([
-            'host' => $host,
-            'user' => $user,
-            'pass' => $pass,
-            'port' => '8729',
-        ]);
         
-        $query = new Query('/system/identity/getall');
-
-        $identity = $client->query($query)->read();
-
-        $this->identity = $identity[0]['name'];
-
-        $query = new Query('/system/resource/print');
-
-        $response = $client->query($query)->read();
-        // La variable $response contendrá un array con la información de los usuarios
-
-        $claves = array_keys($response[0]);
-
-        //dd($response[0]);
-        $texto = '';
-
-        foreach ($response[0] as $key => $elemento) {
-            $texto .= $key .': '. $elemento . '<br>'; //PHP_EOL;
-        }
-
-        $this->response = $texto;
 
     }
 
@@ -81,7 +76,6 @@ class Integracion extends Component
         $ip_del_dominio = gethostbyname($dominio);
 
         return view('livewire.mikrotik.integracion', [
-            'arreglo' => $this->response,
             'ip_del_dominio'  => $ip_del_dominio,
         ]);
     }
--- resources/views/livewire/mikrotik/integracion.blade.php
+++ resources/views/livewire/mikrotik/integracion.blade.php
@@ -1,16 +1,19 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="row">
+        <div class="col-md-12">
+                <h1>Integración del Router: {{ $identity}}</h1>
+                <button wire:click.prevent="integracion()" class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Obtener</button>
+        </div>
+    </div>
     <div class="row container">
         <div class="col-md-12 col-12">
-            <h1>Integración del Router: {{ $identity}}</h1>
-
             {!! $response !!}
         </div>
     </div>
 
-    <div class="row">
+    <div class="row my-3">
         <div class="col-md-12 col-12">
-            {{ $ip_del_dominio }}
+            Ip del dominio: {{ $ip_del_dominio }}
         </div>
     </div>
     
