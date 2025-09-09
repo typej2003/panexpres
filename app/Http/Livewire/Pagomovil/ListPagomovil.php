0e069f88e027587ed852e724a4cc6c466c55440e
Typej <typej2003@gmail.com>
2025-09-09 13:07:56 -0400

    arreglar

--- app/Http/Livewire/Pagomovil/ListPagomovil.php
+++ app/Http/Livewire/Pagomovil/ListPagomovil.php
@@ -258,8 +258,7 @@ class ListPagomovil extends AdminComponent
 
         $pago = Pagomovil::create($data);
 
-        //$message = 'Un usuario realizo un pago movil. Telefono: ' . $data->telefono . ' Referencia: ' . $data->referencia . ' Monto: ' . $data->monto;
-        $message = 'Un usuario realizo un pago movil. Data: ' . json_encode($data);
+        $message = 'Un usuario realizo un pago movil. Telefono: ' . $data['telefono'] . ' Referencia: ' . $data['referencia'] . ' Monto: ' . $data['monto'];
         $sender = new SmsSender;
         $sender->callSendSms($user, $message);
 
