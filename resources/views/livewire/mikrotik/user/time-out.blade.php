4f27c8192cc9791ca0a404df8a2a1849974c7e9c
Typej <typej2003@gmail.com>
2025-09-19 11:44:47 -0400

    null

--- resources/views/livewire/mikrotik/user/time-out.blade.php
+++ resources/views/livewire/mikrotik/user/time-out.blade.php
@@ -67,7 +67,7 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                @if($profilesUser)
+                                @if($usersHotspot)
                                 @foreach ($usersHotspot as $key => $array) 
                                     <div class="col-md-3 col-4">
                                         <div class="card w-100 shadow">
