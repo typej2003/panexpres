09c1c09436e79e7fe6ea2441362f84266ecc42c2
Typej <typej2003@gmail.com>
2025-10-11 09:49:43 -0400

    router

--- app/Http/Controllers/Api/MikrotikPasarelaController.php
+++ app/Http/Controllers/Api/MikrotikPasarelaController.php
@@ -200,8 +200,6 @@ class MikrotikPasarelaController extends Controller
 			
 			$this->router = Router::where('nrorouter', $nrorouter)->first();
 
-			dd($this->router);
-
 			$client = $this->configRouter();
 
 			$password = $this->randomPassword();
