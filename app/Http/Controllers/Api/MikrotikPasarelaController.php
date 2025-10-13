5aaca013a24ec2cc9912a0eae7ccf0e4c567ff08
Typej <typej2003@gmail.com>
2025-10-13 15:35:06 -0400

    pasarela

--- app/Http/Controllers/Api/MikrotikPasarelaController.php
+++ app/Http/Controllers/Api/MikrotikPasarelaController.php
@@ -254,7 +254,7 @@ class MikrotikPasarelaController extends Controller
                         'password' => $userMikrotik->password,
                         'status' => true,
                     ];
-				$userMikrotik->update(['profile'=>$profile]);
+				$userMikrotik->update(['profile'=>$profile, 'active' => false]);
 				$mikrotik_id = $userMikrotik->mikrotik_id;
 
 				// Modificar profile
