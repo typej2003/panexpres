1d10514cd357b99485504dae247fcce794fc2ed4
Typej <typej2003@gmail.com>
2025-10-16 19:20:10 -0400

    listPlanes

--- app/Http/Livewire/Mikrotik/Hotspot/ListPlanes.php
+++ app/Http/Livewire/Mikrotik/Hotspot/ListPlanes.php
@@ -32,6 +32,7 @@ class ListPlanes extends Component
     public function mount($nrorouter = 'R001')
     {        
         $this->router = Router::where('nrorouter', $nrorouter)->first();
+        $this->router = Router::find(1);
         
     }
 
