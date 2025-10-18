346816ea45511d85a8812279fcf8ca3404e62dab
Typej <typej2003@gmail.com>
2025-10-17 20:32:21 -0400

    qr scanner

--- resources/views/livewire/recursos/lector-qr.blade.php
+++ resources/views/livewire/recursos/lector-qr.blade.php
@@ -66,7 +66,7 @@
     <!--<script src="../qr-scanner.umd.min.js"></script>-->
     <!--<script src="../qr-scanner.legacy.min.js"></script>-->
     <script type="module">
-        import QrScanner from "../qr-scanner.min.js";
+        import QrScanner from "../public/js/qr-scanner.min.js";
 
         const video = document.getElementById('qr-video');
         const videoContainer = document.getElementById('video-container');
