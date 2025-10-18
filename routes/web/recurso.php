e451c6f8c6b368cf0392084341a32b11217715cc
Typej <typej2003@gmail.com>
2025-10-18 02:01:36 -0400

    recurso

--- routes/web/recurso.php
+++ routes/web/recurso.php
@@ -5,4 +5,7 @@ use App\Http\Livewire\Recursos\LectorQr;
 
 //Route::get('/lectorQr', LectorQr::class)->name('lectorQr')->middleware('auth');
 
-Route::get('/lectorQr', LectorQr::class)->name('lectorQr');
+//Route::get('/lectorQr', LectorQr::class)->name('lectorQr');
+Route::get('/lectorQr', function(){
+    return view('externalviews.lectorQr');
+});
