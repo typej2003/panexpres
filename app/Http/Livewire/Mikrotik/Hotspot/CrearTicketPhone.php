49a8465358cc0f3a77129d9a7a92d6d78a25852f
Typej <typej2003@gmail.com>
2025-10-18 16:18:09 -0400

    ticketphone

--- app/Http/Livewire/Mikrotik/Hotspot/CrearTicketPhone.php
+++ app/Http/Livewire/Mikrotik/Hotspot/CrearTicketPhone.php
@@ -186,9 +186,7 @@ class CrearTicketPhone extends Component
 
 				$response = $client->query($query)->read();
 
-                dd($mikrotik_id . ' ' . $password . ' ' . $profile);
-
-				$this->cleanUptime($mikrotik_id, $newUptime = "00:00:00");
+                $this->cleanUptime($mikrotik_id, $newUptime = "00:00:00");
 
                 $this->dispatchBrowserEvent('hide-form', ['message' => 'Se han actualizo el usuario ' . $username . ' de Hotspot con éxito.']);
 			}
