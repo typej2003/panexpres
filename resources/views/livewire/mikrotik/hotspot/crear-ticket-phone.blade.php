521d5191c94efa33abeb4208d032bcf249ef3cea
Typej <typej2003@gmail.com>
2025-10-19 01:29:32 -0400

    aa

--- resources/views/livewire/mikrotik/hotspot/crear-ticket-phone.blade.php
+++ resources/views/livewire/mikrotik/hotspot/crear-ticket-phone.blade.php
@@ -180,7 +180,7 @@
                                 <div id="${user['name']}" class="card-body">
                                     <div class="text-start"><span>Usuario: </span>${user['name']}</div>
                                     <div class="text-start"><span>Password: </span>${user['password']}</div>
-                                    <div style="width:100%; height:100%;" id="qr${user['name']}"></div>
+                                    <div style="width:200px; height:200px;" id="qr${user['name']}"></div>
                                 </div>
                                 <div class="card-footer btn-imprimir">
                                     <button onclick="imprimirDiv('${user['name']}')" class="btn btn-success">Imprimir</button>
