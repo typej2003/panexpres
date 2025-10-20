79ee729cc583525fda35b0ece78623ed5efd5555
Typej <typej2003@gmail.com>
2025-10-20 10:56:39 -0400

    pagosatisfactorio

--- resources/views/externalviews/pagosatisfactorioMikrotik.blade.php
+++ resources/views/externalviews/pagosatisfactorioMikrotik.blade.php
@@ -1,7 +1,5 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/app.css">
-
-
 <div class="container-fluid">
     <div>
         <!-- Main content -->
@@ -11,17 +9,13 @@
                     <div class="col-lg-12">
                         <div class="card" style="width: 100% !important;">
                             <div class="card-body text-center">
-                                <h1>Mikrotik</h1>
-
+                                <h3>WifiExprés</h3>
                                 <input type="hidden" id="user" name="user" value="{{ $user }}">
-                                <input type="hidden" id="password" name="password" value="{{ $password }}">                                
-
-                                <h1>Operación procesada con éxito</h1>
+                                <input type="hidden" id="password" name="password" value="{{ $password }}">
+                                <h4>Operación procesada con éxito</h4>
                                 <p>
-                                    Inicio de sesión en <span class="h2 text-danger" id="contador">5 segundos</span>
+                                  Inicio de sesión en <span class="h4 text-danger" id="contador">5 segundos</span>
                                 </p>
-
-                                
                             </div>
                             <div class="card-footer d-flex justify-content-end">
                                 <button class="d-none" onclick="enviarDatoAlPadre()">Enviar Variable al Padre</button>
@@ -35,17 +29,20 @@
                                     // El primer argumento es los datos y el segundo es el origen del padre
                                     window.parent.postMessage(miObjeto, '*'); // Enviar a cualquier origen por simplicidad, pero se recomienda especificar el origen del padre para seguridad.
                                 }
-                                // const idIntervalo = setInterval(() => {
-                                // console.log(contador); // Muestra el valor actual del contador
-                                // document.getElementById('contador').textContent = contador
+                                var contador = 5
+                                function cuentaRegresiva(contador){                                    
+                                    const idIntervalo = setInterval(() => {
+                                        console.log(contador); // Muestra el valor actual del contador
+                                        document.getElementById('contador').textContent = contador
 
-                                // if (contador === 0) {
-                                //     clearInterval(idIntervalo); // Detiene el intervalo cuando llega a 0
-                                //     console.log("¡Cuenta regresiva terminada!");
-                                // } else {
-                                //     contador--; // Disminuye el contador en 1
-                                // }
-                                // }, 1000); // 1000 milisegundos = 1 segundo
+                                        if (contador === 0) {
+                                            clearInterval(idIntervalo); // Detiene el intervalo cuando llega a 0
+                                            console.log("¡Cuenta regresiva terminada!");
+                                        } else {
+                                            contador--; // Disminuye el contador en 1
+                                        }
+                                    }, 1000); // 1000 milisegundos = 1 segundo                                
+                                }
 
                                 const timeoutId = setTimeout(() => {
                                     enviarDatoAlPadre()
