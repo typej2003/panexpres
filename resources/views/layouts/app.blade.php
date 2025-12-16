94022228affcf421fa48df40f9057628601a2e48
typej2003 <typej2003@gmail.com>
2025-12-16 14:23:22 -0400

    aaa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,11 @@
     @stack('styles')
     <livewire:styles />
 
+    @stack('js')
+    @stack('before-livewire-scripts')
+    <livewire:scripts />
+    @stack('after-livewire-scripts')
+
 </head>
     
     <style>
@@ -79,10 +84,7 @@
 <!-- <script src="/js/bootstrap.min.js"></script>
 <script src="/js/bootstrap.bundle.min.js"></script>
 <script src="/js/jquery-3.6.4.min.js"></script> -->
-@stack('js')
-@stack('before-livewire-scripts')
-<livewire:scripts />
-@stack('after-livewire-scripts')
+
 
 @stack('alpine-plugins')
 <!-- Alpine Core -->
