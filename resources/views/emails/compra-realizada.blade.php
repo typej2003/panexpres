7f8b60697266a472013b73184c3af6f5737973f5
typej2003 <typej2003@gmail.com>
2025-12-17 20:16:00 -0400

    email

--- resources/views/emails/compra-realizada.blade.php
+++ resources/views/emails/compra-realizada.blade.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <title>{{ $title }}</title>
-    <link rel="stylesheet" href="/css/styles_expres.css">
+    <link rel="stylesheet" href="{{ asset('/css/styles_expres.css') }}">
 </head>
 <body>
     <div class="text-center mb-4">
-        <img src="/img/logopanexpres_color.png" alt="Logo Pan Express" style="max-height: 100px; width: auto;">
+        <img src="{{ asset('/img/logopanexpres_color.png') }}" alt="Logo Pan Express" style="max-height: 100px; width: auto;">
         
         <hr class="my-3 mx-auto" style="width: 80%; border: 0; border-top: 5px solid #893e08; opacity: 1;">
     </div>
