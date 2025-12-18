e5c714c9d8f7135cf1d907f96ce9cc6ebb3a1ec2
typej2003 <typej2003@gmail.com>
2025-12-17 20:28:48 -0400

    email

--- resources/views/emails/compra-realizada.blade.php
+++ resources/views/emails/compra-realizada.blade.php
@@ -2,22 +2,56 @@
 <html>
 <head>
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/styles_expres.css') }}">
 </head>
 <body>
     <div class="text-center mb-4">
         <img src="{{ asset('/img/logopanexpres_color.png') }}" alt="Logo Pan Express" style="max-height: 100px; width: auto;">
-        
+        <br>
         <hr class="my-3 mx-auto" style="width: 80%; border: 0; border-top: 5px solid #893e08; opacity: 1;">
     </div>
     
-    <h1>Hola, {{ $names }} {{ $surnames }}</h1>
+    <!-- <h1>Hola, {{ $names }} {{ $surnames }}</h1>
     <p>¡Gracias por tu compra!</p>
     <p>{!! $body !!}</p>
     <p>Te invitamos a disfrutar todos nuestros productos</p>
     <p><button class="btn btn-success">Sigue tu pedido</button></p>
     <p>Rastrea, reprograma o cancela tu pedido</p>
     <p>Gracias,</p>
-    {{ config('app.name') }}<br>
+    {{ config('app.name') }}<br> -->
+
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="text-center mb-4">
+                                <img src="{{ asset('/img/logopanexpres_color.png') }}" alt="Logo Pan Express" style="max-height: 100px; width: auto;">
+                                <br>
+                                <hr class="my-3 mx-auto" style="width: 80%; border: 0; border-top: 5px solid #893e08; opacity: 1;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <h1>Hola, {{ $names }} {{ $surnames }}</h1>
+                            <p>¡Gracias por tu compra!</p>
+                            <p>{!! $body !!}</p>
+                            <p>Te invitamos a disfrutar todos nuestros productos</p>
+                            <p><button class="btn btn-success">Sigue tu pedido</button></p>
+                            <p>Rastrea, reprograma o cancela tu pedido</p>
+                            <p>Gracias,</p>
+                            {{ config('app.name') }}<br>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
