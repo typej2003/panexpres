80a57c4ecefd2a0f499685ae692d9affec3c59cd
typej2003 <typej2003@gmail.com>
2025-12-19 06:25:04 -0400

    aaa

--- resources/views/externalviews/checkout.blade.php
+++ resources/views/externalviews/checkout.blade.php
@@ -1,3 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/app.css">
 <link rel="stylesheet" href="/css/styles_expres.css">
