357e043a32a352322a788578e4b0abdf5b1e1ff0
typej2003 <typej2003@gmail.com>
2025-12-19 13:40:58 -0400

    aa

--- resources/views/livewire/cliente/detalles-pedido.blade.php
+++ resources/views/livewire/cliente/detalles-pedido.blade.php
@@ -43,7 +43,7 @@
                                 <span>Situación de Pago: </span>
                             </div>
                             <div class="col-lg-6">
-                                <span class="p-3 {{ $this->getClaseConfirmed($pedido->confirmed) }}">{{ $pedido->getConfirmed() }}</span>
+                                <div class="p-3 {{ $this->getClaseConfirmed($pedido->confirmed) }}">{{ $pedido->getConfirmed() }}</div>
                             </div>
                         </div>
                         <div class="row">
@@ -51,7 +51,7 @@
                                 <span>Status: </span>
                             </div>
                             <div class="col-lg-6">
-                                <span class="p-3 {{ $this->getClaseStatus($pedido->status) }}">{{ $pedido->getStatus() }}</span>
+                                <div class="p-3 {{ $this->getClaseStatus($pedido->status) }}">{{ $pedido->getStatus() }}</div>
                             </div>
                         </div>
                         <div class="row">
