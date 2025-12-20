73e9e92f7e3e9a3d3f6d6bead5968db696a6943a
typej2003 <typej2003@gmail.com>
2025-12-20 09:56:59 -0400

    aa

--- resources/views/livewire/afiliado/product/repuestoexpres/new-product-r-e.blade.php
+++ resources/views/livewire/afiliado/product/repuestoexpres/new-product-r-e.blade.php
@@ -5,9 +5,9 @@
                 <div class="col-sm-6">
                         <h1 class="m-0 text-dark">
                         @if($controlActivity)
-                            <span>Editar Producto RepuestoExpres</span>
+                            <span>Editar Producto PanExpres</span>
                         @else
-                            <span>Crear Producto RepuestoExpres</span>
+                            <span>Crear Producto PanExpres</span>
                         @endif
                     </h1>
                 </div><!-- /.col -->
