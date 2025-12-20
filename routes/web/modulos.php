802f346011fdb86fa0c09531272e6807beadc9e7
typej2003 <typej2003@gmail.com>
2025-12-20 03:19:58 -0400

    eere

--- routes/web/modulos.php
+++ routes/web/modulos.php
@@ -145,6 +145,6 @@ Route::get('/agendar', function (Request $request) {
 
 // Ejemplo en routes/web.php
 // Route::post('/cita-guardar', [FormularioCita::class, 'store'])->name('cita.guardar');
-Route::GET('/agendar.cita', Cita::class)->name('agendar.cita');
+Route::GET('/agendarcita', Cita::class)->name('agendar.cita');
 
 Route::get('/recomendados', ShowRecommendedExpres::class)->name('recomendados');
