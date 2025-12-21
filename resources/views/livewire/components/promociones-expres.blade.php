cff010ff0d08575a0ed621c1b9e4113dd218835b
typej2003 <typej2003@gmail.com>
2025-12-21 08:50:51 -0400

    aaa

--- resources/views/livewire/components/promociones-expres.blade.php
+++ resources/views/livewire/components/promociones-expres.blade.php
@@ -111,11 +111,11 @@
                                 <img src="{{ $promocion->avatar_url }}" alt="{{ $promocion->name }}">
                             </a>
                             <div class="promo-overlay">
-                                @if($promocion->product->in_offer == '1')
+                                <!-- @if($promocion->product->in_offer == '1')
                                     {{$currencyValue}}. {{ $promocion->product->price_offer }} <span class="tachado">antes {{ $promocion->product->price1 }}</span>
                                 @else
                                     {{$currencyValue}}. {{ $promocion->product->price1 }}
-                                @endif
+                                @endif -->
                             </div>
                         @else
                             <img src="{{ $promocion->avatar_url }}" alt="{{ $promocion->name }}">
