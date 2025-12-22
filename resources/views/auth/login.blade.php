74eb979d48ea0c46bce3bac9ead59941a058be83
typej2003 <typej2003@gmail.com>
2025-12-22 00:06:30 -0400

    eee

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PanExpres</title>
-  <link rel="shortcut icon" type="x-icon" href="/img/panexpres_logo.png" />
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Compra y vende Pan de Jamón y Combos Navideños Caracas">
+    <meta name="keywords" content="vende, compra, Pan, Jamón, Pan de Jamón, Combos Navideños, Caracas, directo a tu mesa, @panexpres.vezla, Panexprés, Panexpres">
+    <link rel="icon" type="image/png" href="{{ asset('android-chrome-192x192.png?v=1') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico?v=1') }}">
+    <meta name="theme-color" content="#ff572f"> 
+
+    <title>PanExprés</title>
+    <title>@yield('title', 'PanExpres - Tu panadería a un clic')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
