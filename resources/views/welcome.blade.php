f4011c593c7fb7bffe17061af44055f7b98947a8
typej2003 <typej2003@gmail.com>
2025-12-21 20:42:11 -0400

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Compra y vende Pan de Jamón y Combos Navideños Caracas">
     <meta name="keywords" content="vende, compra, Pan, Jamón, Pan de Jamón, Combos Navideños, Caracas, directo a tu mesa, @panexpres.vezla, Panexprés, Panexpres">
-    <link rel="shortcut icon" type="x-icon" href="/img/android-chrome-512x512.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('android-chrome-192x192.png') }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('android-chrome-192x192.png?v=1') }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico?v=1') }}">
+    <meta name="theme-color" content="#ff572f"> ```
 
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
 
     <title>PanExprés</title>
+    <title>@yield('title', 'PanExpres - Tu panadería a un clic')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('/css/styles_expres.css') }}">
