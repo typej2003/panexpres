30c0193d21a3bf5fec320826cd0a964118327b35
typej2003 <typej2003@gmail.com>
2025-12-21 20:35:22 -0400

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Compra y vende Pan de Jamón y Combos Navideños Caracas">
     <meta name="keywords" content="vende, compra, Pan, Jamón, Pan de Jamón, Combos Navideños, Caracas, directo a tu mesa, @panexpres.vezla, Panexprés, Panexpres">
-    <link rel="shortcut icon" type="x-icon" href="/img/panexpres_logo.png" />
+    <link rel="shortcut icon" type="x-icon" href="/img/android-chrome-512x512.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('android-chrome-192x192.png') }}">
+    
     <title>PanExprés</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
