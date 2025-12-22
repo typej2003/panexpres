62544bef816fbd0fc1b2e9fe854aa9f390990855
typej2003 <typej2003@gmail.com>
2025-12-22 00:03:41 -0400

    aefe

--- resources/views/externalviews/checkout.blade.php
+++ resources/views/externalviews/checkout.blade.php
@@ -1,5 +1,13 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Compra y vende Pan de Jamón y Combos Navideños Caracas">
+    <meta name="keywords" content="vende, compra, Pan, Jamón, Pan de Jamón, Combos Navideños, Caracas, directo a tu mesa, @panexpres.vezla, Panexprés, Panexpres">
+    <link rel="icon" type="image/png" href="{{ asset('android-chrome-192x192.png?v=1') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico?v=1') }}">
+    <meta name="theme-color" content="#ff572f"> 
+
+    <title>PanExprés</title>
+    <title>@yield('title', 'PanExpres - Tu panadería a un clic')</title>
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/app.css">
 <link rel="stylesheet" href="/css/styles_expres.css">
