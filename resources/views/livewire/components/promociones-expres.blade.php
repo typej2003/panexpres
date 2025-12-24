e05e062d20823dc92c44cfb68dcff12d23645bd7
typej2003 <typej2003@gmail.com>
2025-12-24 11:35:20 -0400

    ok

--- resources/views/livewire/components/promociones-expres.blade.php
+++ resources/views/livewire/components/promociones-expres.blade.php
@@ -112,11 +112,7 @@
                             </a>
                             <div class="promo-overlay">
                                 @if($promocion->product->in_offer == '1')
-                                    <!-- @if($promocion->product->nroproductdelivery >= 3)
-                                        <span> +3 panes delivery gratis</span>
-                                    @else -->
-                                        {{$currencyValue}}. {{ $promocion->product->price_offer }} <span class="tachado">antes {{ $promocion->product->price1 }}</span>
-                                    <!-- @endif -->
+                                    {{$currencyValue}}. {{ $promocion->product->price_offer }} <span class="tachado">antes {{ $promocion->product->price1 }}</span>
                                 @else
                                     {{$currencyValue}}. {{ $promocion->product->price1 }}
                                 @endif 
