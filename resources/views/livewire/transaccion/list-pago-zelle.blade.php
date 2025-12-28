fbb528569e864b11da03701eb5c5be289282be67
typej2003 <typej2003@gmail.com>
2025-12-27 23:10:24 -0400

    subir

--- resources/views/livewire/transaccion/list-pago-zelle.blade.php
+++ resources/views/livewire/transaccion/list-pago-zelle.blade.php
@@ -33,3 +33,88 @@
         {{ $pagos->links() }}
     </div>
 </div>
+
+    <div wire:poll.10s> {{-- Se actualiza automáticamente cada 10 segundos --}}
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Pagos Zelle</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/admin/dashboard">Escritorio</a></li>
+                            <li class="breadcrumb-item active">Pagos Zelle</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="d-flex justify-content-between mb-2">
+                            <button wire:click.prevent="addNew" class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Nuevo Usuario</button>
+                            <x-search-input wire:model="searchTerm" />
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table table-hover table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">
+                                                Fecha
+                                            </th>
+                                            <th scope="col">
+                                                Remitente
+                                            </th>
+                                            <th scope="col">Monto</th>
+                                            <th scope="col">Referencia</th>
+                                            <th scope="col">Memo</th>
+                                            <th scope="col">Estado</th>
+                                            <th scope="col">Opciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody wire:loading.class="text-muted">
+                                        @forelse ($pagos as $index => $pago)
+                                        <tr>
+                                            <th scope="row">{{ $pagos->firstItem() + $index }}</th>
+                                            <td>{{ $pago->fecha_pago }}</td>
+                                            <td>{{ $pago->remitente }}</td>
+                                            <td>${{ number_format($pago->monto, 2) }}</td>
+                                            <td>{{ $pago->referencia }}</td>
+                                            <td class="text-xs">{{ $pago->nota_memorandum }}</td>
+                                            <td>
+                                                <span class="px-2 py-1 text-xs font-semibold text-green-800 bg-green-100 rounded-full">
+                                                    {{ $pago->estado }}
+                                                </span>    
+                                            </td>
+                                        </tr>
+                                        @empty
+                                        <tr class="text-center">
+                                            <td colspan="5">
+                                                <img src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/v2/assets/empty.svg" alt="No results found">
+                                                <p class="mt-2">No se encontro resultados</p>
+                                            </td>
+                                        </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="card-footer d-flex justify-content-end">
+                                {{ $pagos->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+
+    </div>
